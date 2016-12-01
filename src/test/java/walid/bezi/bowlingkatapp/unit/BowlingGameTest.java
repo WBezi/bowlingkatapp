129d2a7cee136766f43a215d6f0526cf6e2943e1
WBezi <bezi.walid@gmail.com>
2016-12-01 22:39:42 +0100

    first commit

---
+++ src/test/java/walid/bezi/bowlingkatapp/unit/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package walid.bezi.bowlingkatapp.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
